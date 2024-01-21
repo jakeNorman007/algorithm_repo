96e5fe2e9ad391d03d61033b7f9a08fd1cd18e52
jakeNorman007 <jacob.a.norman91@gmail.com>
2024-01-21 08:56:43 -0500

    .

--- src/key/Linear_search.ts
+++ src/key/Linear_search.ts
@@ -1,4 +1,6 @@
 export default function linear_search(foo: number[], bar: number): boolean {
+    //running time for linear search in O(N)
+    
     for(let i = 0; i < foo.length; ++i) {
         if(foo[i] === bar) {
             return true;
---
+++ src/workbook/Linear_Search_Wokrbook.ts
@@ -0,0 +1,3 @@
+export default function linear_search_list_wb(foo: number[], bar: number): boolean {
+    return true;
+}
